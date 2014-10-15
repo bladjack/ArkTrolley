4afe3f566326c83a48733c1cc7382260867a2022
hakim-ali <hakimalisami@gmail.com>
2014-10-15 18:55:45 +0500

    Create the Architecture for the application.
    Create the samples application of android and IOS ... And run the application

---
+++ ArkTrolley.Android.Core/App.cs
@@ -0,0 +1,21 @@
+using Android.Content;
+using Cirrious.CrossCore.IoC;
+using Cirrious.CrossCore.Platform;
+using Cirrious.MvvmCross.Droid.Platform;
+using Cirrious.MvvmCross.ViewModels;
+
+namespace ArkTrolley.Android.Core
+{
+    public class App : Cirrious.MvvmCross.ViewModels.MvxApplication
+    {
+        public override void Initialize()
+        {
+            CreatableTypes()
+                .EndingWith("Service")
+                .AsInterfaces()
+                .RegisterAsLazySingleton();
+
+            RegisterAppStart<ViewModels.FirstViewModel>();
+        }
+    }
+}
---
+++ ArkTrolley.iOS.Core/App.cs
@@ -0,0 +1,21 @@
+using Cirrious.CrossCore.IoC;
+using MonoTouch.UIKit;
+using Cirrious.CrossCore.Platform;
+using Cirrious.MvvmCross.ViewModels;
+using Cirrious.MvvmCross.Touch.Platform;
+
+namespace ArkTrolley.iOS.Core
+{
+    public class App : Cirrious.MvvmCross.ViewModels.MvxApplication
+    {
+        public override void Initialize()
+        {
+            CreatableTypes()
+                .EndingWith("Service")
+                .AsInterfaces()
+                .RegisterAsLazySingleton();
+
+            RegisterAppStart<ViewModels.FirstViewModel>();
+        }
+    }
+}
